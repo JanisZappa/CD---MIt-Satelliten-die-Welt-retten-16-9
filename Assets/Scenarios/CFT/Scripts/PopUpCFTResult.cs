0aa16d17b076f494da4da0d3c4b72e926dda6820
JanisZappa <schoenau.a@googlemail.com>
2025-11-20 12:28:38 +0100

    skip

--- Assets/Scenarios/CFT/Scripts/PopUpCFTResult.cs
+++ Assets/Scenarios/CFT/Scripts/PopUpCFTResult.cs
@@ -43,7 +43,10 @@ public class PopUpCFTResult : PopUp
         
         float min = Mathf.Min(a, Mathf.Min(b, c));
         float max = Mathf.Max(a, Mathf.Max(b, c));
-        float range = 1f / (max - min);
+        
+        float a2 = Mathf.Approximately(a, max) ? 1 : Mathf.Approximately(a, min) ? .3f : .65f;
+        float b2 = Mathf.Approximately(b, max) ? 1 : Mathf.Approximately(b, min) ? .3f : .65f;
+        float c2 = Mathf.Approximately(c, max) ? 1 : Mathf.Approximately(c, min) ? .3f : .65f;
 
         float speed = .45f * max;
         while (t < max)
@@ -51,16 +54,14 @@ public class PopUpCFTResult : PopUp
             t += Time.deltaTime * speed;
 
             float l = Mathf.Clamp(t, 0, max);
-            
-            rects[0].localScale = new Vector3(Mathf.Min(a, l), 1, 1);
-            rects[1].localScale = new Vector3(Mathf.Min(b, l), 1, 1);
-            rects[2].localScale = new Vector3(Mathf.Min(c, l), 1, 1);
+            rects[0].localScale = new Vector3(Mathf.SmoothStep(0, a, a > 0? Mathf.Min(a, l) / a : 0), 1, 1);
+            rects[1].localScale = new Vector3(Mathf.SmoothStep(0, b, b > 0? Mathf.Min(b, l) / b : 0), 1, 1);
+            rects[2].localScale = new Vector3(Mathf.SmoothStep(0, c, c > 0? Mathf.Min(c, l) / c : 0), 1, 1);
 
-            float tMax = Mathf.Min(max, l);
-        
-            images[0].color = barTex.GetPixelBilinear(Mathf.Clamp01(a / tMax * range - min), 0);
-            images[1].color = barTex.GetPixelBilinear(Mathf.Clamp01(b / tMax * range - min), 0);
-            images[2].color = barTex.GetPixelBilinear(Mathf.Clamp01(c / tMax * range - min), 0);
+            float l2 = l / max;
+            images[0].color = barTex.GetPixelBilinear(Mathf.Lerp(1, a2, l2), 0);
+            images[1].color = barTex.GetPixelBilinear(Mathf.Lerp(1, b2, l2), 0);
+            images[2].color = barTex.GetPixelBilinear(Mathf.Lerp(1, c2, l2), 0);
             
             yield return null;
         }
